57b621ea410123b129493b6727b1e9b9d1cddf8f
dshaw0004 <114005178+dshaw0004@users.noreply.github.com>
2023-09-22 19:31:01 +0530

    robot text integration add

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
-import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-
 import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://example.com",
-	integrations: [mdx(), sitemap()],
+	site: "https://dshaw0004.netlify.com",
+	integrations: [mdx(), sitemap(), robotsTxt()],
 	experimental: {
 		viewTransitions: true,
 	},
